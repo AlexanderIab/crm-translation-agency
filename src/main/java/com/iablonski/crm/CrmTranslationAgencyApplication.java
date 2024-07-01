5c10263ac03b429a47f4203ba5252e41a25c844d
Alexander_I <alex.iablonski@gmail.com>
2024-07-01 21:34:09 +0200

    Initial commit

---
+++ src/main/java/com/iablonski/crm/CrmTranslationAgencyApplication.java
@@ -0,0 +1,13 @@
+package com.iablonski.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmTranslationAgencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrmTranslationAgencyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/iablonski/crm/CrmTranslationAgencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iablonski.crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmTranslationAgencyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
